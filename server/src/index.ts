3cc4aafea456da9b0086da5fdc2e5a84653cc7a2
moMedhat <mmr973320@gmail.com>
2024-02-06 23:30:04 +0200

    connect to mongodb and make the models

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,36 @@
 import express , { Request,Response } from "express";
 import dotenv from 'dotenv';
 import { sampleProducts } from "./data";
+import mongoose from "mongoose";
 dotenv.config()
 
 /** constants */
 const app = express()
-const PORT = process.env.PORT
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/shopo'
+const PORT:number = parseInt((process.env.PORT) as string,10)
 
 app.get('/api/products',(req:Request,res:Response)=>{
     res.json(sampleProducts);
 })
+
+
 /** middlewares  */
 
 
 
+/* connect to Mongodb */
 
+mongoose.set('strictQuery',true)
+mongoose.connect(MONGO_URL)
+.then(()=>{
+    console.log("Db is connecting :)")
+})
+.catch((e)=>{
+    console.log(`error in db :( ${e}`);
+})
+
+
+/** Run the server */
 app.listen(PORT || 5000, ()=>{
     console.log(`Server is running on port ${PORT} :)`);
 })
