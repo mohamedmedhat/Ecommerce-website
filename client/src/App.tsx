4f312ec5a5fbf473516623a467887ca22e12d1e9
moMedhat <mmr973320@gmail.com>
2024-02-07 19:46:06 +0200

    prepare pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Signin from "./pages/SignIn/Signin";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
